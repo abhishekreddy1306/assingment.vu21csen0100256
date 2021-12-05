34cd248c33d23ce57ef11a46cd149ca3fff6e3b9
abhishekreddy1306 <93900639+abhishekreddy1306@users.noreply.github.com>
2021-12-05 22:31:00 +0530

    Create area_of_ellipse.c

---
+++ area_of_ellipse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+  float a,b;
+  printf("enter the length of the minor raddius and major raddius respectively");
+  scanf("%f%f",&a,&b);
+
+  printf("the area of the ellipse%f",3.14*a*b);
+  return 0;
+}
