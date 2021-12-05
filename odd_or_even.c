25d4f67c49e579e9aeb33e0eeb3f28cea8d61ee4
abhishekreddy1306 <93900639+abhishekreddy1306@users.noreply.github.com>
2021-12-05 22:45:47 +0530

    Create odd_or_even.c

---
+++ odd_or_even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+ int a;
+ printf("Enter the any value :");
+ scanf("%d",&a);
+
+ if(a%2==0){
+ printf("The entered value is even");
+ }else{
+ printf("The entered value is odd");
+ }
+ return 0;
+}
