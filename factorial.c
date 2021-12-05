fa3c02c95a66e11542c873e03eb1f620ca5638aa
abhishekreddy1306 <93900639+abhishekreddy1306@users.noreply.github.com>
2021-12-05 22:37:16 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+ int a,j=1;
+ printf("Enter the value for the factorial");
+ scanf("%d",&a);
+
+ for(int i=1; i<=a; i++){
+    j *= i;
+ }
+ printf("%d",js);
+
+
+ return 0;
+
+}
